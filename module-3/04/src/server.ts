8d2a4f2a606b65f123f84c2fc9ee1cf35ccef656
Bryan Karta <karta.bryan@gmail.com>
2024-03-26 22:10:02 +0700

    update-materi

--- module-3/04/src/server.ts
+++ module-3/04/src/server.ts
@@ -2,24 +2,41 @@ import express, { Request, Response, Application } from "express";
 import { PoolConnection, QueryError } from "mysql2";
 import db from "./config/db";
 
+interface User {
+  id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  age: number;
+}
+
 const PORT: number = 8080;
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get("/products", (req: Request, res: Response) => {
-  interface Product {
-    id: number;
-    name: string;
-    description: string;
-    stock: number;
-    price: number;
-  }
-  db.query("SELECT * FROM product", (err: QueryError, result: Product[]) => {
+app.get("/users", (req: Request, res: Response) => {
+  db.query("SELECT * FROM user", (err: QueryError, result: User[]) => {
     if (err) return res.status(500).send(err);
-    return res.status(200).send(result);
+    return res.status(200).json({
+      message: "Get user success",
+      data: result,
+    });
   });
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  const { username, first_name, last_name, age }: User = req.body;
+  db.query(
+    `INSERT INTO user(username, first_name, last_name, age)
+  values("${username}", "${first_name}", "${last_name}", "${age}")`,
+    (err: QueryError, result: User) => {
+      console.log(result);
+      if (err) return res.status(500).send(err);
+      return res.status(200).send(result);
+    }
+  );
 });
 
 db.getConnection(
